ed40638cadd64776e1ae572d29b6172be0de26d2
Paul Abraham <abrahamspaa@gmail.com>
2018-05-04 06:23:55 +0530

    added gitigonre

--- back-end/routes/login.js
+++ back-end/routes/login.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', function(req, res, next) {
-	let user_id = req.body.username;
-    let password = req.body.password;
+router.post('/', (request, response, next) => {
+	let username  = request.body.username;
+	let password  = request.body.password;
+console.log(1)
 
-    console.log(req);
-    console.log(password);
-
-    res.json([{
-        id: 1,
-        accessToken: "token"
-    }]);
+	if (username && password && username === password) {
+		console.log(2)
+	  response.json([{
+	      id: 1,
+	      accessToken: "token"
+	  }]);
+	} else {
+		response.setHeader('WWW-Authenticate', 'Basic realm="need login"');
+		console.log('No authorization found, send 401.');
+		response.send(401);
+	}
 });
 
 module.exports = router;
--- front-end/src/views/Login/index.js
+++ front-end/src/views/Login/index.js
@@ -4,21 +4,21 @@ import './style.css';
 export default class Login extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             username: '',
             password: ''
         };
-    
+
         this.onUserNameChange = this.onUserNameChange.bind(this);
         this.onPasswordChange = this.onPasswordChange.bind(this);
         this.onLogin = this.onLogin.bind(this);
     }
-    
+
     /**
      * On change of user name state will be set
-     * 
-     * @param {Object} event 
+     *
+     * @param {Object} event
      */
     onUserNameChange(event) {
         this.setState({username: event.target.value});
@@ -27,19 +27,22 @@ export default class Login extends Component {
 
     /**
      * On change of password will be set in state
-     * 
-     * @param {Object} event 
+     *
+     * @param {Object} event
      */
     onPasswordChange(event) {
         this.setState({password: event.target.value});
     }
-    
+
     onLogin(event) {
         alert('A name was submitted: ' + this.state.username);
         event.preventDefault();
 
         fetch('login', {
             method: 'post',
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8'
+            },
             body: JSON.stringify(this.state)
           }).then(function(response) {
             alert(response);
@@ -48,7 +51,7 @@ export default class Login extends Component {
             console.log('Created Gist:', data.html_url);
           });
     }
-    
+
     render() {
         return (
             <form onSubmit={this.onLogin}>
@@ -63,4 +66,3 @@ export default class Login extends Component {
         );
     }
 }
-
